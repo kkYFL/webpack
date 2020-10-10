316f7b2531bb2b226220552f283f0149877981bb
杨丰林 <1490719714@qq.com>
2020-10-10 17:00:17 +0800

    配置样式-css、postcss、scss、less   补充

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './css/reset.css';
+import './scss/public.scss';
+import './less/index.less';
 document.getElementById('app').innerHTML = "Webpack works";
 document.getElementById('postcss').innerHTML = "<h1>我自动添加了浏览器前缀</h1>";
--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -4,64 +4,123 @@ const open = require('opn');//打开浏览器
 const chalk = require('chalk');// 改变命令行中输出日志颜色插件
 
 module.exports = {
-    // 入口文件配置项
-    entry: path.resolve(__dirname, 'src/index.js'),
-    // 输出文件配置项
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: ""
-    },
-    // webpack4.x 环境配置项
-    mode:"development",
-        // 插件配置项
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',//输出文件的名称
-            template: path.resolve(__dirname, 'src/index.html'),//模板文件的路径
-            title:'webpack-主页',//配置生成页面的标题
-        }),
-    ],
-    
-    // 开发服务配置项
-    devServer: {
-        port: 8080,
-        contentBase: path.resolve(__dirname, 'dist'),
-        historyApiFallback: true,
-        host: '0.0.0.0',
-        overlay:true,
-        after() {
-            open('http://localhost:' + this.port)
-            .then(() => {
-                console.log(chalk.cyan('成功打开链接： http://localhost:' + this.port));
-            })
-            .catch(err => {
-                console.log(chalk.red(err));
-            });
-        }
-    },
+  // 入口文件配置项
+  entry: path.resolve(__dirname, 'src/index.js'),
+  // 输出文件配置项
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: ""
+  },
+  // webpack4.x 环境配置项
+  mode: "development",
+  // 插件配置项
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',//输出文件的名称
+      template: path.resolve(__dirname, 'src/index.html'),//模板文件的路径
+      title: 'webpack-主页',//配置生成页面的标题
+    }),
+  ],
+
+  // 开发服务配置项
+  devServer: {
+    port: 8080,
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    overlay: true,
+    after () {
+      open('http://localhost:' + this.port)
+        .then(() => {
+          console.log(chalk.cyan('成功打开链接： http://localhost:' + this.port));
+        })
+        .catch(err => {
+          console.log(chalk.red(err));
+        });
+    }
+  },
 
 
-    // 加载器 loader 配置项
-    module:{
-        rules:[
-            {
-                test: /\.css$/,
-                use: [{
-                        loader: 'style-loader'
-                    },{
-                        loader: 'css-loader'
-                    },{
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            config: {
-                                path: 'postcss.config.js'
-                            }
-                        }
-                    }
-                ]
-            }]
+  // 加载器 loader 配置项
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+            config: {
+              path: 'postcss.config.js'
+            }
+          }
         },
-    };
+        {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        }
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+};
 
