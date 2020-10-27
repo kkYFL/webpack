4f8aaece7de4fb8f44728c31c9a4f9bcb6e45779
杨丰林 <1490719714@qq.com>
2020-10-27 15:42:24 +0800

    配置图片、文件、图标文字---备注：htmlloader配置有区别，因为上面配置出现问题，音视频没有测试

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './css/reset.css';
-import './scss/public.scss';
+// import './scss/public.scss';
 import './less/index.less';
+import image from './images/favicon.png';
+
+
+
 document.getElementById('app').innerHTML = "Webpack works";
+// document.getElementById('postcss').innerHTML = "<h1>我自动添加了浏览器前缀</h1><img src=" + image + "></img>";
 document.getElementById('postcss').innerHTML = "<h1>我自动添加了浏览器前缀</h1>";
--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -119,7 +119,65 @@ module.exports = {
             }
           }
         ]
-      }
+      },
+      {
+        test: /\.(png|jp?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,        // 小于8192字节的图片打包成base 64图片
+              name: 'images/[name].[hash:8].[ext]',
+              publicPath: ''
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      },
+      {
+        // 文件依赖配置项——字体图标
+        test: /\.(woff|woff2|svg|eot|ttf)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            limit: 8192,
+            name: 'fonts/[name].[ext]?[hash:8]',
+            publicPath: ''
+          },
+        }],
+      }, {
+        // 文件依赖配置项——音频
+        test: /\.(wav|mp3|ogg)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            limit: 8192,
+            name: 'audios/[name].[ext]?[hash:8]',
+            publicPath: ''
+          },
+        }],
+      }, {
+        // 文件依赖配置项——视频
+        test: /\.(ogg|mpeg4|webm)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            limit: 8192,
+            name: 'videos/[name].[ext]?[hash:8]',
+            publicPath: ''
+          },
+        }],
+      },
     ]
   },
 };
