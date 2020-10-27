a4f9f04b55ec2de6837744137a47311690a5ff95
杨丰林 <1490719714@qq.com>
2020-10-27 15:45:03 +0800

    频繁存在需要关闭终端，重开项目才能成功运行项目的情况，测试是缓存问题？？？

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import './css/reset.css';
 import './less/index.less';
 import image from './images/favicon.png';
 
-
-
 document.getElementById('app').innerHTML = "Webpack works";
 // document.getElementById('postcss').innerHTML = "<h1>我自动添加了浏览器前缀</h1><img src=" + image + "></img>";
 document.getElementById('postcss').innerHTML = "<h1>我自动添加了浏览器前缀</h1>";
